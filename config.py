96059fe0eed74fd885d6ae9470a631c5f70b793d
shookshire <destinnsy@gmail.com>
2020-08-10 23:46:36 +0800

    First commit

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 BOT_CONFIG = {
-	'service_account_json_path': 'D:/TelegramBot/test/service_account.json',
-	'spreadsheet_id': '1f5dkmYH0ihK4T1SMBXNrCyIFR6Csp4KEZhLpofYuA3g',
-	'bot_token': '1306022391:AAHPU_jdsROozle-r28LtQLbYB7TaUHVT1I'
+	'bot_token': ''
 }
 
 DB_CONFIG = {
-	'db_host':"localhost",
-	'db_database':"riichi",
-	'db_user':"postgres",
-	'db_password':"Asdf1234"
+	'db_host':"",
+	'db_database':"",
+	'db_user':"",
+	'db_password':""
 }
